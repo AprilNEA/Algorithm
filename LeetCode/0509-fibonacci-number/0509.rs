fa1c7257985779f4e3aeaf3c68bff10b0e913bb3
AprilNEA <github@sku.moe>
2024-06-08 21:01:40 +0800

    leetcode dp 1

---
+++ LeetCode/0509-fibonacci-number/0509.rs
@@ -0,0 +1,13 @@
+pub fn fib(n: i32) -> i32 {
+    if n == 1 {
+        return 1
+    }
+
+    let mut dp = vec![0; (n+1) as usize];
+    dp[0] = 1;
+    dp[1] = 1;
+    for i in 2..=n as usize {
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+    dp[n as usize]
+}
